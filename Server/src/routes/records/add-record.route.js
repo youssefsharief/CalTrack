bee6727d18454449095dc1ad43d4a9278f2b9294
Youssef Sherif <sharief@aucegypt.edu>
2018-05-03 06:08:35 +0200

    Integrate nutritionix

--- Server/src/routes/records/add-record.route.js
+++ Server/src/routes/records/add-record.route.js
@@ -1,11 +1,16 @@
 const addRecord = require('../../data-layer/add-record.db')
+const { getNutriCalories } = require('../../external-apis/nutritionix')
 
 
-module.exports = (req, res, next) => {
-    const date = new Date(req.body.date)
+module.exports = async (req, res, next) => {
+    const dateWithTimezone = new Date(req.body.date)
+    const date = new Date(dateWithTimezone.getTime() - dateWithTimezone.getTimezoneOffset() * 60 * 1000)
+    const numOfCalories = req.body.numOfCalories ? req.body.numOfCalories : await getNutriCalories(req.body.name)
     return addRecord(req.params.id, {
         name: req.body.name,
-        numOfCalories: req.body.numOfCalories,
-        date: new Date(date.getTime() - date.getTimezoneOffset() * 60 * 1000)
+        numOfCalories,
+        date
     }).catch(err => next(err)).then(user => res.status(200).json(user))
 }
+
+
