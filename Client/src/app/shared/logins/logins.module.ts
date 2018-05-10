bfcca2a06bb53b9214bc12e3b83ad9f57ba04a82
Youssef Sherif <sharief@aucegypt.edu>
2018-05-10 05:54:10 +0200

    Update UI style

--- Client/src/app/shared/logins/logins.module.ts
+++ Client/src/app/shared/logins/logins.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { LoginsComponent } from './logins.component';
 import { CommonModule } from '@angular/common';
+import { SharedTrimmedModule } from 'app/shared/shared-trimmed.module';
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule, SharedTrimmedModule
     ],
     declarations: [
         LoginsComponent,
