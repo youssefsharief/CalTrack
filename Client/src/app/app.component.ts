8466f74cd3f75ae49f644126c664a2fdb093b7a8
Youssef Sherif <sharief@aucegypt.edu>
2018-05-14 11:45:41 +0200

    Refresh user profile after route change

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'app/core/services/data.service';
+import { AuthService } from 'app/core/services/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
+    private dataService: DataService,
+    private authService: AuthService,
+    private router: Router
   ) { }
+
+  ngOnInit() {
+    this.refreshUserProfileAfterRouteChange()
+  }
+
+  private refreshUserProfileAfterRouteChange() {
+    this.router.events.filter(event => (event instanceof NavigationEnd)).subscribe((routeData: NavigationEnd) => {
+      const excludedRoutes = ['/signup', '/signup/activate', '/signup/success', '/login', '/email_password_recovery',
+        '/email_password_recovery/submit_new_password', '/email_password_recovery/password_recovered_successfully']
+      if (!excludedRoutes.includes(routeData.urlAfterRedirects)) {
+        this.fetchForData()
+      }
+    })
+  }
+
+  private fetchForData() {
+    this.dataService.getUserDetails(this.authService.getId()).subscribe(
+      data => this.authService.saveProfile(data)
+    )
+  }
+
+
 }
