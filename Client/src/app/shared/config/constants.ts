f7db2b91c012b1359bd9905e2fa0fb8c613cefe0
Youssef <sharief@aucegypt.edu>
2020-08-09 02:52:43 -0700

    Update facebook client id

--- Client/src/app/shared/config/constants.ts
+++ Client/src/app/shared/config/constants.ts
@@ -7,6 +7,6 @@ export const codeErrorMessage = 'Please enter a valid code'
 export const caloriesErrorMessage = 'Please enter a number between 0 and 3000 for calories'
 export const maxCaloriesErrorMessage = 'Please enter a number between 500 and 8000 for calories'
 export const roles = ['manager', 'admin', 'regular']
-export const facebookClientId = '218590022238538'
+export const facebookClientId = '2700957473449013'
 export const googleClientId = '170990127407-hmj1dvgspn0vbn6bn1q284a9j36eh6mj.apps.googleusercontent.com'
 export const captchaSiteKey = '6Lcl0lcUAAAAACWH461TX1rI5j4ULpQajhTnM_uY'
